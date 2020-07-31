47783899b2bc81b3d6eaf99e198b2d70a1e91935
Rodrigo Aramburu <rodrigoaramburu@gmail.com>
2020-07-31 18:02:03 -0300

    correção url

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 const URL_BACKEND = window.location.hostname.includes('localhost')
   ? 'http://localhost:8080'
-  : 'https://www.botecodigital.info/react-api/';
+  : 'https://www.botecodigital.info/react-api';
 
 export default URL_BACKEND;
